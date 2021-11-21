9fab1571d13d9288f8ff7474959e4283d0650652
jssjmnzsnts <happycode.inbox@gmail.com>
2021-11-20 22:00:53 -0800

    Initial Commit

---
+++ EasySell/EasySellApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasySellApp.swift
+//  EasySell
+//
+//  Created by Jesus Jimenez on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct EasySellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
